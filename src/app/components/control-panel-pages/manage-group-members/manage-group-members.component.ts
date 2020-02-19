fedcbc61b6a2d25d042a92c6cc3bc2acb07ccbf5
Billy Bissic <billy@rpl.org>
2020-02-19 16:54:47 -0600

    updating manage group members file with styles for the checkboxes and adding scroll bars to the unassigned users div as well as creating select all and clear selection toggle functionality

--- src/app/components/control-panel-pages/manage-group-members/manage-group-members.component.ts
+++ src/app/components/control-panel-pages/manage-group-members/manage-group-members.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ControlPanelAccessService} from '../../../services/control-panel-access-service/control-panel-access.service';
 import {UserGroup} from '../../../interfaces/control-panel-access/user-group.interface';
 import {User} from '../../../interfaces/control-panel-access/user.interface';
+import {MatSelectionList} from '@angular/material';
 
 @Component({
   selector: 'app-manage-group-members',
@@ -14,6 +15,11 @@ export class ManageGroupMembersComponent implements OnInit {
   currentUserGroup: UserGroup = null;
   unassignedUsers: User[] = [];
   assignedUsers: User[] = [];
+
+  isAllUnassignedSelected: Boolean = false;
+  isAllAssignedSelected: Boolean = false;
+  selectAllUnassignedButtonText: String = 'Select All';
+  selectAllAssignedButtonText: String = 'Select All';
 
   constructor(private controlPanelAccessService: ControlPanelAccessService,
               private changeDetectorRef: ChangeDetectorRef) { }
@@ -48,5 +54,29 @@ export class ManageGroupMembersComponent implements OnInit {
   private getGroupInformation(group: UserGroup) {
     this.currentUserGroup = group;
     this.assignedUsers = this.currentUserGroup.users;
+  }
+
+  public toggleUnassignedUsersSelection(unassignedusers: MatSelectionList) {
+    if (this.isAllUnassignedSelected === false) {
+      this.isAllUnassignedSelected = true;
+      this.selectAllUnassignedButtonText = 'Clear All';
+      unassignedusers.selectAll();
+    } else if (this.isAllUnassignedSelected === true) {
+      this.isAllUnassignedSelected = false;
+      this.selectAllUnassignedButtonText = 'Select All';
+      unassignedusers.deselectAll();
+    }
+  }
+
+  public toggleAssignedUserSelection(assignedusers: MatSelectionList) {
+    if (this.isAllAssignedSelected === false) {
+      this.isAllAssignedSelected = true;
+      this.selectAllAssignedButtonText = 'Clear All';
+      assignedusers.selectAll();
+    } else if (this.isAllAssignedSelected === true) {
+      this.isAllAssignedSelected = false;
+      this.selectAllAssignedButtonText = 'Select All';
+      assignedusers.deselectAll();
+    }
   }
 }
